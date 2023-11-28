e9d6ca2dc048cd4541c6f945c99d54dcdecc32fb
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2023-11-28 22:44:15 +0100

    Release 1.0.0

--- src/main/java/me/oondanomala/hfix/HFix.java
+++ src/main/java/me/oondanomala/hfix/HFix.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 public class HFix {
     public static final String MODID = "hfix";
     public static final String NAME = "hFix";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
     public static final Logger LOGGER = LogManager.getLogger("hFix");
     @Mod.Instance(MODID)
     public static HFix instance = new HFix();
