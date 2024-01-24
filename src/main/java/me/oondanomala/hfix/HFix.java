3aabf618c1fdbfd626d39e599bd2cc958245c842
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2024-01-24 19:29:28 +0100

    Use mod name constant for logger

--- src/main/java/me/oondanomala/hfix/HFix.java
+++ src/main/java/me/oondanomala/hfix/HFix.java
@@ -16,7 +16,7 @@ public class HFix {
     public static final String MODID = "hfix";
     public static final String NAME = "hFix";
     public static final String VERSION = "1.0.0";
-    public static final Logger LOGGER = LogManager.getLogger("hFix");
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
     @Mod.Instance(MODID)
     public static HFix instance = new HFix();
     public static Config config;
