3de745f1c99645c04f4b9049394153cdca33690c
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2024-04-19 19:43:15 +0200

    Bump version

--- src/main/java/me/oondanomala/hfix/HFix.java
+++ src/main/java/me/oondanomala/hfix/HFix.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 public class HFix {
     public static final String MODID = "hfix";
     public static final String NAME = "hFix";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     @Mod.Instance(MODID)
     public static HFix instance;
