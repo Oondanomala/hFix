714c3e6afca545779ee4d40223b6da2b318b9ef5
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2025-11-06 02:43:16 +0100

    Switch to Essential loom
    Also updates Gradle to 8.14.3 and the GitHub actions

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    idea
+    java
+    id("gg.essential.loom") version "1.10.+"
+}
+
+val modID: String by project
+val modName: String by project
+val modGroup: String by project
+val modVersion: String by project
+val mcVersion = "1.8.9"
+
+group = modGroup
+version = modVersion
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+loom {
+    runConfigs {
+        remove(getByName("server"))
+    }
+
+    forge {
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
+        accessTransformer(rootProject.file("src/main/resources/${modID}_at.cfg"))
+    }
+
+    // For some reason loom defaults to tab indentation
+    decompilers {
+        named("vineflower") {
+            options.put("indent-string", "    ")
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.8.9")
+    mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
+    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
+}
+
+sourceSets.main {
+    output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory })
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    processResources {
+        inputs.property("modID", modID)
+        inputs.property("modName", modName)
+        inputs.property("version", version)
+        inputs.property("mcVersion", mcVersion)
+
+        filesMatching(listOf("mcmod.info")) {
+            expand(inputs.properties) {
+                escapeBackslash = true
+            }
+        }
+
+        rename("(.+_at.cfg)", "META-INF/$1")
+    }
+
+    jar {
+        manifest.attributes(mapOf(
+            "FMLAT" to "${modID}_at.cfg"
+        ))
+    }
+}
